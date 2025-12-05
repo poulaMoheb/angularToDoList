83e439ba2f51afcc005988d594ff0302e358ddae
pmoheb <pmoheb@isolvedhcm.com>
2025-12-05 16:03:21 +0200

    adding local storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -28,7 +28,13 @@ export class TasksService{
         },
       ];
 
-    
+    constructor(){
+      const tasks = localStorage.getItem('tasks');
+
+      if(tasks){
+        this.tasks = JSON.parse(tasks);
+      }
+    }
     getUserTasks(userId:string){
         return this.tasks.filter((task)=> task.userId === userId)
     }
@@ -41,9 +47,15 @@ export class TasksService{
           summary:task.summary,
           dueDate:task.date,
           })
+          this.saveTasks();
     }
 
     removeTask(id: string) {
-        return this.tasks = this.tasks.filter(task=> task.id === id);
+        this.tasks = this.tasks.filter(task=> task.id === id);
+        this.saveTasks();
+    }
+
+    private saveTasks(){
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
     }
 }
